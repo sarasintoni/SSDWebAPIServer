5d29cfab47c680034adb80f2099eaf60be51f295
Sara Sintoni <sara.sintoni4@studio.unibo.it>
2017-11-13 10:56:11 +0100

    first commit

---
+++ WebAPIServer/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http.Headers;
+
+namespace WebAPIServer
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //{action} lo abbiamo aggiunto noi per specificare quale metodo vogliamo invocare 
+            //(e NON capiscitelo da solo)
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // per far restituire json e non xml
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ WebAPIServer/Controllers/OrdiniController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPIServer.Models;
+using System.Web.Http;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace WebAPIServer.Controllers
+{
+    public class OrdiniController : ApiController
+    {
+
+        [HttpGet]                // in esecuzione solo con un get dal client
+        [ActionName("GetAllOrders")]   // nome del metodo esposto nella API
+        public string GetAllOrders()
+        {
+            string connString;
+            List<ordini> lstOrdini = null;
+
+            /*connString = ConfigurationManager.ConnectionStrings["SQLiteConn"].ConnectionString;
+            Persist P = new Persist();
+            lstOrdini = P.readTableViaI(connString, true, 0);
+
+            Persist P = null;*/
+            return JsonConvert.SerializeObject(lstOrdini);
+        }
+    }
+}
