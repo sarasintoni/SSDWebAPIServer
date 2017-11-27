20f2997fe02a0880b33064f317403aff64e79d43
Sara Sintoni <sara.sintoni4@studio.unibo.it>
2017-11-27 11:02:44 +0100

    inizio euristiche

---
+++ WebAPIServer/js/globalVar.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+var DEBUG = true; //enable/disable console log
+if(!DEBUG) { console.log = function() {}}
+
+
+var n;   // num clienti
+var m;   // num server
+var c;   // matrice dei costi
+var req; // matrice delle richieste
+var cap; // vattore delle capacità
+var sol; // vettore soluzione
+var solbest; // vettore migliore soluzione trovata
+var startTime, endTime, timeDiff; // tempi esecuzione
+var zub = Number.MAX_VALUE;       // costo miglior soluzione trovata = z upper bound
+var zlbBest = Number.MAX_VALUE;   // best lower bound
+
+var jInstance;    // istanza in input da risolvere in notazione json
+var EPS = 0.001;
---
+++ WebAPIServer/js/loadGap.js
@@ -0,0 +1,59 @@
+﻿function readWithCORS() {
+    var resp;
+    var req = new XMLHttpRequest();
+    inText = document.getElementById("inputRemote").value;
+    if ('withCredentials' in req) {
+        req.open('GET', 'http://astarte.csr.unibo.it/gapdata/' + inText, true); //invia la richiesta al server
+        req.onreadystatechange = function () {
+            if (req.readyState === 4)
+                if (req.status >= 200 && req.status < 400) {
+                    jInstance = JSON.parse(req.responseText);
+                    setInstance(jInstance);
+                }
+                else
+                    alert('reading error');
+        };
+        req.send();
+    }
+}
+
+
+// elaborazione dei dati letti in locale
+function receivedText(e) {
+    istanza = e.target.result;
+    jInstance = JSON.parse(istanza);
+    setInstance(jInstance);
+}
+
+//copia i campi del json => poi ho tutte le variabili che mi servono
+function setInstance(jInstance) {
+    n = jInstance.numcustomers;   // num clienti
+    m = jInstance.numfacilities;  // num server
+    c = jInstance.cost;   // matrice dei costi
+    req = jInstance.req;  // matrice delle richieste
+    cap = jInstance.cap;  // vattore delle capacità
+    alert("Got instance " + jInstance.name + " n=" + n);
+}
+
+function checkSol(sol) {
+    var z = 0, j;
+    var capused = new Array(m);
+    for (i = 0; i < m; i++) capused[i] = 0;
+    // controllo assegnamenti
+    for (j = 0; j < n; j++)
+        if (sol[j] < 0 || sol[j] >= m || sol[j] === undefined) {
+            z = Number.MAX_VALUE;
+            return z;
+        }
+        else
+            z += c[sol[j]][j];
+    // controllo capacità
+    for (j = 0; j < n; j++) {
+        capused[sol[j]] += req[sol[j]][j];
+        if (capused[sol[j]] > cap[sol[j]]) {
+            z = Number.MAX_VALUE;
+            return z;
+        }
+    }
+    return z;
+}
